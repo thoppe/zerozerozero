7d0391e1540de42a89ef6e44e712be9a1baf6f74
thoppe <travis.hoppe@gmail.com>
2019-08-13 10:20:31 -0400

    Add per file option

--- P0_find_zeros.py
+++ P0_find_zeros.py
@@ -7,14 +7,15 @@ import sys, os
 import h5py
 import scipy.stats
 
-save_dest = 'data/point_clouds'
-os.system(f'mkdir -p {save_dest}')
+save_dest = "data/point_clouds"
+os.system(f"mkdir -p {save_dest}")
 
 fps = 60
 n_bases = 6
 
 N = 7
-n_samples = 25000
+n_samples = 25000 * 4
+
 np_sample_seed = 26
 
 
@@ -23,9 +24,9 @@ def interpolate(n, frames_per):
 
     low_s = 0.05
     high_s = 1.00
-    
-    sigma = np.random.uniform(low=low_s, high=high_s, size=[n,])   
-    G = [scipy.stats.norm(loc=k, scale=s) for k,s in zip(range(n), sigma)]
+
+    sigma = np.random.uniform(low=low_s, high=high_s, size=[n])
+    G = [scipy.stats.norm(loc=k, scale=s) for k, s in zip(range(n), sigma)]
 
     ZX = []
     for frame_idx, t in tqdm(enumerate(T), total=len(T)):
@@ -45,16 +46,18 @@ def interpolate(n, frames_per):
 def random_coeffs(N):
     return np.random.normal(size=(N,))
 
+
 def get_roots(real=None, imag=None):
-        
+
     if real is None:
         real = random_coeffs(N)
     if imag is None:
         imag = random_coeffs(N)
 
-    coeffs = real + imag*1J
-    
-    p = mp.polyroots(coeffs, maxsteps=100, extraprec=110)
+    coeffs = real + imag * 1j
+
+    # p = mp.polyroots(coeffs, maxsteps=100, extraprec=110)
+    p = mp.polyroots(coeffs, maxsteps=50, extraprec=20)
     p = [[float(z.real), float(z.imag)] for z in p]
     return np.array(p)
 
@@ -64,7 +67,7 @@ def sample(seed=None):
 
     if seed is not None:
         np.random.seed(seed)
-    
+
     while r is None:
         try:
             r = get_roots(real_coeffs)
@@ -77,17 +80,18 @@ def sample(seed=None):
 
 func = joblib.delayed(sample)
 
+
 def compute_set(real_coeffs):
     np.random.seed(np_sample_seed)
 
     roots = []
 
     with joblib.Parallel(-1) as MP:
-    
+
         ITR = np.random.randint(0, np.iinfo(np.int16).max, size=(n_samples,))
-        for res in MP(func(seed=x) for x in ITR):
+        for res in MP(func(seed=x) for x in tqdm(ITR)):
             roots.extend(res)
-            
+
     return np.array(roots)
 
 
@@ -95,7 +99,6 @@ C_BASE = [random_coeffs(N) for _ in range(n_bases)]
 C_BASE.append(C_BASE[0])
 C_BASE = np.array(C_BASE)
 
-f_save = os.path.join(save_dest, 'points.h5')
 
 points = []
 base_points = []
@@ -104,32 +107,46 @@ T = []
 ITR = enumerate(zip(C_BASE, C_BASE[1:]))
 weights = interpolate(len(C_BASE), fps)
 
-ITR = zip(np.linspace(0, 1, len(C_BASE)*fps), weights)
+ITR = zip(np.linspace(0, 1, len(C_BASE) * fps), weights)
+
+
+frame_n = 0
+
 for t, w in tqdm(ITR, total=len(weights)):
 
-    real_coeffs = (C_BASE.T*w).sum(axis=1)
-    
+    f_save = os.path.join(save_dest, f"points_{frame_n:06d}.h5")
+    if os.path.exists(f_save):
+
+        frame_n += 1
+        continue
+
+    real_coeffs = (C_BASE.T * w).sum(axis=1)
+
     T.append(t)
 
     base = get_roots(real_coeffs, np.zeros_like(real_coeffs))
-    real, imag = base.T
-    base_points.append([real, imag])
-    
+    # real, imag = base.T
+    # base_points.append([real, imag])
+
     roots = compute_set(real_coeffs)
-    real, imag = roots.T
-    points.append([real, imag])
+    # real, imag = roots.T
+    # points.append([real, imag])
 
-with h5py.File(f_save, 'w') as h5:
-    h5['C_BASE'] = C_BASE
-    
-    h5['points'] = points
-    h5['base_points'] = base_points
-    h5['t'] = T
+    with h5py.File(f_save, "w") as h5:
 
+        h5["poly_real"] = real_coeffs
+        h5["poly_weights"] = w
+        h5["C_BASE"] = C_BASE
+        h5.attrs["t"] = t
 
+        h5["points"] = roots
+        h5["base_points"] = base
 
+    print(f_save)
 
-'''
+    frame_n += 1
+
+"""
 #plt.scatter(real, imag, s=10, alpha=0.75, color='r')
 plt.scatter(real, imag, s=1, alpha=0.25)
 
@@ -143,12 +160,12 @@ plt.savefig(f'figures/{k:03d}.png')
 
 plt.clf()
 plt.cla()
-'''
+"""
 
-#plt.show()
+# plt.show()
 
 
-'''
+"""
 import pixelhouse as ph
 cx = ph.Canvas(800, 800)
 
@@ -159,4 +176,4 @@ for r in tqdm(roots):
     
     cx += ph.filters.gaussian_blur(blur_x=0.01, blur_y=0.01)
 cx.show()
-'''
+"""
--- P1_draw_static.py
+++ P1_draw_static.py
@@ -3,105 +3,70 @@ import h5py, os
 import matplotlib
 import numpy as np
 from functools import partial
+import pixelhouse as ph
 
-#circle_radius = 0.001
+
 circle_radius = 0.0005
+pal = ph.palette(0)
+bg_color = np.array(pal[0]) / 40
+
+# canvas = ph.Canvas(800, 800, bg=bg_color, extent=2)
+canvas = ph.Canvas(1280, 1280, bg=bg_color, extent=2.25)
+
+save_dest = "ph_figures"
+os.system(f"mkdir -p {save_dest}")
+
+cutoff = 10 ** 4
 
 
-save_dest = 'ph_figures'
-os.system(f'mkdir -p {save_dest}')
-f_save = os.path.join('data/point_clouds/', 'points.h5')
+def get_frame(k):
 
-with h5py.File(f_save, 'r') as h5:
-    
-    #points = h5['points'][...]
-    points = h5['points'][:, :, :]
-    print(points.shape)
+    f_save = os.path.join(f"data/point_clouds/", f"points_{k:06d}.h5")
+    if not os.path.exists(f_save):
+        raise ValueError(f"Cant find {f_save}")
 
-    base_points = h5['base_points'][...]
-    t = h5['t'][...]
+    with h5py.File(f_save, "r") as h5:
+
+        points = h5["points"][:cutoff, ...]
+
+        base_points = h5["base_points"][...]
+        t = h5.attrs["t"]
+
+    return points, base_points, t
+
 
 ##################################################################
 
-import pixelhouse as ph
-pal = ph.palette(0)
-
-bg_color = np.array(pal[0])/40
-print(bg_color)
-#bg_color = 'k'
-
-canvas = ph.Canvas(width=1280, height=1280, bg=bg_color, extent=3.0)
 
 # Identify the pairs
-print(base_points.shape)
-yp = base_points[0][1]
+_, base_points, _ = get_frame(0)
+
+
+yp = base_points.T[1]
 idx = np.argsort(np.argsort(yp))
-c0 = np.where(np.isin(idx,[0,5]))[0]
-c1 = np.where(np.isin(idx,[1,4]))[0]
-c2 = np.where(np.isin(idx,[2,3]))[0]
+c0 = np.where(np.isin(idx, [0, 5]))[0]
+c1 = np.where(np.isin(idx, [1, 4]))[0]
+c2 = np.where(np.isin(idx, [2, 3]))[0]
+
 cidx = np.array([c0[0], c0[1], c1[0], c1[1], c2[0], c2[1]])
-frame = 0
 
-prior_bp = None
-from scipy.spatial.distance import cdist
-#from pycpd import affine_registration,deformable_registration
 
-#def point_cloud_affine(iteration, error, X, Y):
-#    print("HERE", iteration, error, X, Y)
-#    print("EXXX\n",X)
-#callback = partial(point_cloud_affine)
+for frame_n in range(0, 10000):
 
-for bp, pt in tqdm(zip(base_points, points)):
-    
-#for bp, pt in zip(base_points, points):
+    # f_save = f'ph_figures/{frame_n:06d}.png'
+    # if os.path.exists(f_save):
+    #    print(f"Skipping {f_save}")
+    #    continue
 
-    '''
-    if prior_bp is not None:
-        #help(deformable_registration)
-        reg = deformable_registration(X=prior_bp, Y=bp)
-        reg.register(callback)
-        #print(reg)      
-        #print("HI")
-        #print(dir(reg))
-        #print(reg.D, reg.G)
-        #print(reg.M, reg.N)
-        #print(reg.Np, reg.P)
-        #print(reg.P1, reg.Pt1)
-        #print(reg.TY.shape, reg.W.shape)
-        #print(reg.X.shape, reg.Y.shape)
-        #print(prior_bp)
-        #print(bp)
-        #print((bp-prior_bp).round(2))
-        #print(bp.round(2))
-        #print(prior_bp.round(2))
-        #print((reg.W-bp).round(2))
-        #print(reg.W.round(3))
-        
-        #exit()
-        
-        
-        #exit()
-        dist = cdist(prior_bp.T, bp.T)
-        #np.fill_diagonal(dist, 100)
+    try:
+        pt, bp, t = get_frame(frame_n)
+    except ValueError:
+        break
 
-        idx = []
-        for i, row in enumerate(dist):
-            j = None
-            row = row.tolist()
-            while j is None or j in idx:
-                j = np.argmin(row)
-            
+    bp = bp.T
+    pt = pt.T
 
-        
-        idx = np.argmin(dist, axis=0)
-        cidx = cidx[idx]
-        #print(dist)
-        #print(dist.shape)
-        print(idx)      
-        #exit()
-    '''
-    
-
+    """
     BP = np.array([
 
         [bp[0][cidx[0]], bp[1][cidx[0]]],
@@ -114,87 +79,66 @@ for bp, pt in tqdm(zip(base_points, points)):
         [bp[0][cidx[5]], bp[1][cidx[5]]],
 
     ])
+    """
 
-    
+    COLORS = [pal[1], pal[1], pal[2], pal[2], pal[3], pal[3]]
 
-    COLORS = [
-        pal[1], pal[1],
-        pal[2], pal[2],
-        pal[3], pal[3],
-    ]
+    COLORS = [pal[1]] * 6
 
-    COLORS = [pal[1],]*6
-    
-
-    '''
+    """
     for _ in range(20):
         canvas += ph.filters.gaussian_blur(0.5, 0.5)
-    '''
-    
-    # Draw the centroids as a light blurry dus
-    for (x,y), c in zip(BP, COLORS):
-       canvas += ph.circle(x, y, r=0.025, color=pal[4])
-    canvas += ph.filters.gaussian_blur(0.4, 0.4)
-    for (x,y), c in zip(BP, COLORS):
-        canvas += ph.circle(x, y, r=0.025, color=pal[4])
+    """
 
-    
     def draw_color_points(canvas):
-        for q in pt.T[:]:
-            w = np.exp(-2*np.linalg.norm(BP-q,axis=1))
-            w /= w.sum()
-            w = (w.reshape(-1,1)*COLORS).sum(axis=0)
-            c = np.clip(w, 0, 255).astype(np.uint8)
+
+        c = np.array(COLORS[0])
+        c_dim = c // 10
+
+        img = canvas.img.astype(np.float)
+        print(img.sum())
+
+        for q in tqdm(pt.T[:]):
 
             px = canvas.transform_x(q[0])
             py = canvas.transform_y(q[1])
 
-            if px >= canvas.shape[0]:
+            if px >= canvas.shape[1]:
                 continue
 
-            if py >= canvas.shape[1]:
+            if py >= canvas.shape[0]:
                 continue
 
-            if(px<0 or py<0):
+            if px < 0 or py < 0:
                 continue
-            
-            canvas.img[px, py] = c
 
-            #canvas.img[px+1, py] = c
-            #canvas.img[px-1, py+1] = c
+            img[px, py] += c_dim
+            # img[px, py]  = np.max
 
-            #canvas += ph.circle(q[0], q[1], r=circle_radius, color=c.tolist())
+        img = np.clip(img, 0, 255).astype(np.uint8)
+        canvas.img = img
+        print(img.sum())
 
-    
-    #print(canvas.transform_kernel_length(0.02))
-    #print(canvas.transform_kernel_length(0.0000000001))
-    #exit()
-    
     with canvas.layer() as C2:
         draw_color_points(C2)
-        #C2 += ph.filters.gaussian_blur(0.02, 0.02)
+        # C2 += ph.filters.gaussian_blur(0.02, 0.02)
+        # draw_color_points(C2)
 
-        #with C2.layer() as C3:
-        #    draw_color_points(C3)
-        #    #print(C3)
-        #    #exit()
-            
+    # Draw the centroids a light blurry glow
+    with canvas.layer() as C2:
+        for (x, y), c in zip(bp.T, COLORS):
+            C2 += ph.circle(x, y, r=0.025, color=pal[4])
+        C2 += ph.filters.gaussian_blur(0.4, 0.4)
+        for (x, y), c in zip(bp.T, COLORS):
+            C2 += ph.circle(x, y, r=0.025, color=pal[4])
 
-    canvas.img = canvas.img[128*2:-128*2, :]
-    
-    print(canvas.shape)
-    canvas.save('demo.png')
-    canvas.resize(0.5).show()
-
-    exit()
-    
-    #if frame < 30:
-    canvas.save(f'ph_figures/{frame:03d}.png')
-    frame += 1
-
-    #canvas.show(1)
     canvas.show()
     exit()
+
+    output = canvas.copy()
+    output.img = output.img[128:-128, :]
+    output.save(f_save)
+    output.show(1)
+
     canvas = canvas.blank()
     prior_bp = bp
-
