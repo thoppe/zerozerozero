dce942b136d56e4390af0bbf30a0c073f270fe06
Travis Hoppe <travis.hoppe@gmail.com>
2019-08-20 23:55:47 -0400

    Add joblib version

--- buddha/old_src/P0_find_zeros.py
+++ buddha/old_src/P0_find_zeros.py
@@ -1,39 +1,39 @@
 import numpy as np
 from tqdm import tqdm
 import joblib
-import sys, os, glob
+import sys, os
 import scipy.signal
 import pixelhouse as ph
 import h5py
-import joblib
-import tempfile
+import ray
+from ray.tune.util import pin_in_object_store, get_pinned_object
 
+#alpha = 1.5
+alpha = 2.0
 
-alpha = 1.5
-#alpha = 2.0
 extent = 1.5
 
-# 3840x2160p is youtube 4K
-# resolution = 1600
+# 3840x2160p is youtube 4K 
+#resolution = 1600
 resolution = 2048
 
 N = 50000
-parallel_iterations = joblib.cpu_count()
-inner_loops = 200
+parallel_iterations = 200
 
-save_dest = "data/points"
-os.system(f"mkdir -p {save_dest}")
+save_dest = 'data/points'
+os.system(f'mkdir -p {save_dest}')
 
-ITERS = [100, 200, 500]
+ray.init()
+
+ITERS = [100,]# 200, 500]
 
 
 def complex_equation(Z, C, alpha):
-    return Z ** alpha + C
-
+    return Z**2 + C
 
 def grid_targets(alpha, iterations):
-    np.warnings.filterwarnings("ignore")
-
+    np.warnings.filterwarnings('ignore')
+    
     # Create a complex grid
     line = np.linspace(-extent, extent, resolution)
     grid = np.meshgrid(line, line)
@@ -44,7 +44,8 @@ def grid_targets(alpha, iterations):
     # are still in the set given a few iterations
     Z = np.zeros_like(C)
     for _ in range(iterations):
-        Z = complex_equation(Z, C, alpha)
+        Z = Z**2 + C
+        #Z = complex_equation(Z, C, alpha)
     in_set = (~np.isnan(Z)).reshape([resolution, resolution])
 
     # Uncomment for a quick viz
@@ -63,7 +64,6 @@ def grid_targets(alpha, iterations):
     zi = C[targets]
 
     return zi
-
 
 def generate_starting_points(C):
 
@@ -90,10 +90,11 @@ def get_iterations(N, alpha, iterations, zi):
     Z = np.zeros_like(C)
 
     data = []
-
-    # for _ in tqdm(range(iterations)):
+    
+    #for _ in tqdm(range(iterations)):
     for _ in range(iterations):
-        Z = complex_equation(Z, C, alpha)
+        Z = Z**2 + C
+        #Z = complex_equation(Z, C, alpha)
         data.append(Z)
 
     # Drop the points that don't escape
@@ -114,101 +115,106 @@ def pts_to_bins(pts, resolution, extent):
     return counts
 
 
-def compute_set(N, alpha, iterations, resolution, extent, seed, save_dest):
-    np.warnings.filterwarnings("ignore")
+def bins_to_image(counts, resolution, boost=1.0):
+
+    #_, bins = np.histogram(counts.ravel(), bins=255)
+    #norm_color = np.digitize(counts, bins, True)
+
+    norm_color = (counts / counts.max())*255
+    img = np.clip(norm_color * boost, 0, 255).astype(np.uint8)
+
+    return img
+
+
+
+@ray.remote
+def compute_set(
+        N, alpha, iterations, resolution, extent,
+        #zi, seed=None
+        zi_obj, seed=None
+        
+):
+    np.warnings.filterwarnings('ignore')
     np.random.seed(seed)
 
-    counts = np.zeros((resolution, resolution), dtype=np.uint64)
+    zi = get_pinned_object(zi_obj)
+        
+    pts = get_iterations(N, alpha, iterations, zi)
+    counts = pts_to_bins(pts, resolution, extent)
+    #print(f"Found {counts.sum()/10**6:0.1f}*10**6 points")
 
-    for _ in range(inner_loops): 
-        pts = get_iterations(N, alpha, iterations, zi)
-        counts += pts_to_bins(pts, resolution, extent)
+    return counts
 
-    f_save = os.path.join(save_dest, f'{seed}.npy')
-    np.save(f_save, counts)
-    # print(f"Found {counts.sum()/10**6:0.1f}*10**6 points")
-    #return None
-    # return counts
+#iterations = 500
+
+#ITERS = [100, 200, 500]
 
 
 for i, iterations in enumerate(ITERS):
 
     # Construct an empty file with the arguments
     f_save = os.path.join(
-        save_dest, f"{iterations}_{resolution}_{alpha:0.5f}.h5"
+        save_dest,
+        f'{iterations}_{resolution}_{alpha:0.5f}.h5',
     )
     if not os.path.exists(f_save):
-        with h5py.File(f_save, "w") as h5:
-            h5.attrs["resolution"] = resolution
-            h5.attrs["iterations"] = iterations
-            h5.attrs["extent"] = extent
-            h5.attrs["alpha"] = alpha
-            h5["counts"] = np.zeros(
-                shape=(resolution, resolution), dtype=np.uint32
-            )
+        with h5py.File(f_save, 'w') as h5:
+            h5.attrs['resolution'] = resolution
+            h5.attrs['iterations'] = iterations
+            h5.attrs['extent'] = extent
+            h5.attrs['alpha'] = alpha
+            h5['counts'] = np.zeros(
+                shape=(resolution,resolution), dtype=np.uint32)
 
-    h5 = h5py.File(f_save, "r+")
+    h5 = h5py.File(f_save, 'r+')
 
-    if "zi" not in h5:
-        print("Finding the boundary")
+    if 'zi' not in h5:
         zi = grid_targets(alpha, iterations)
-        h5["zi"] = zi
+        h5['zi'] = zi
     else:
-        zi = h5["zi"][...]
+        zi = h5['zi'][...]
 
     # Drop the objects into the queue
+    object_ids = []
 
-    func = joblib.delayed(compute_set)
-    with joblib.Parallel(-1
-    ) as MP, tempfile.TemporaryDirectory() as TMP:
+    zi_obj = pin_in_object_store(zi)
+
+    for k in range(parallel_iterations):
+        args = (N, alpha, iterations, resolution, extent, zi_obj)
+        obj = compute_set.remote(*args, seed=k)
+        object_ids.append(obj)
+
+    # Accumulate the results
+    counts = np.zeros((resolution, resolution), dtype=np.uint64)
+
+
+    with tqdm(total=len(object_ids)) as progress:
+        while len(object_ids):
+            obj, object_ids = ray.wait(object_ids, num_returns=1)
+            counts += ray.get(obj[0]).copy()
+            progress.update()
         
-        args = (N, alpha, iterations, resolution, extent)
-        ITR = tqdm(range(parallel_iterations))
-
-        for res in MP(func(*args, seed=k, save_dest=TMP) for k in ITR):
-            pass
-
-        # Accumulate the results
-        counts = np.zeros((resolution, resolution), dtype=np.uint64)
-
-        for f_npy in tqdm(glob.glob(os.path.join(TMP, '*'))):
-            counts += np.load(f_npy)
-
     print(f"Intermediate {counts.sum()/10**6:0.1f}*10**6 points")
 
-    h5["counts"][...] = h5["counts"][...] + counts
-    counts = h5["counts"][...]
+    h5['counts'][...] = h5['counts'][...] + counts
+    counts = h5['counts'][...]
     h5.close()
-
+    
     print(f"Final {counts.sum()/10**6:0.1f}*10**6 points")
-
-##########################################################################
-
-import pixelhouse
-
-def bins_to_image(counts, resolution, boost=1.0):
-
-    # _, bins = np.histogram(counts.ravel(), bins=255)
-    # norm_color = np.digitize(counts, bins, True)
-
-    norm_color = (counts / counts.max()) * 255
-    img = np.clip(norm_color * boost, 0, 255).astype(np.uint8)
-
-    return img
 
 
 canvas = ph.Canvas(resolution, resolution, extent=extent)
 
 for i, iterations in enumerate(ITERS):
     f_save = os.path.join(
-        save_dest, f"{iterations}_{resolution}_{alpha:0.5f}.h5"
+        save_dest,
+        f'{iterations}_{resolution}_{alpha:0.5f}.h5',
     )
-    
-    with h5py.File(f_save, "r") as h5:
-        counts = h5["counts"][...]
+    with h5py.File(f_save, 'r') as h5:
+        counts = h5['counts'][...]
 
-    img = bins_to_image(counts, resolution, 2)
+    img = bins_to_image(counts, resolution, 2)   
     canvas.img[:, :] = img[:, :, np.newaxis]
-    # canvas.img[:, :, i] = img[:, :]
-
+    #canvas.img[:, :, i] = img[:, :]
+    
 canvas.resize(0.5).show()
